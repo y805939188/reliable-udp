c2ed2e966f25f05c325cc0862a5aa389fbc1bd99
dingyubo <805939188@qq.com>
2021-02-13 17:37:33 +0800

    update: rdt2.1

--- rdt2.1/udp-client.js
+++ rdt2.1/udp-client.js
@@ -10,11 +10,16 @@ class ClientFiniteStateMachine {
     RDT_SEND: 'rdt_send',
     IS_ACK: 'is_ack',
     IS_NAK: 'is_nak',
+    NOT_CORRUPT: 'not_corrupt',
+    CORRUPT: 'corrupt',
   };
 
   prev_msg = null;
 
   buffer_queue = [];
+
+  // 设置一个初始序号 该序号要在 0 和 1 之间来回切换
+  current_seq = 0;
 
   constructor({ SEND_INTERVAL, SERVER_PORT, SERVER_ADDRESS, CLIENT_PORT }) {
     if (SEND_INTERVAL && SERVER_PORT && SERVER_ADDRESS && CLIENT_PORT) {
@@ -38,37 +43,28 @@ class ClientFiniteStateMachine {
   // 接收消息
   init_on_message = () => this.udp_client.on('message', (msg, { port, address }) => {
     console.log(`udp 客户端接收到了来自 ${address}:${port} 的消息`);
-    const { flag, data } = JSON.parse(msg);
     // 把上一次发送的 msg 也就是队列最左侧的 msg 先拿出来, 这个 msg 有可能发送成功, 也有可能发送失败
     const prev_msg = this.buffer_queue.shift();
-    if (flag === 'ACK') {
-      console.log(`客户端发送消息后接收到了 ACK 应答, 该分组发送成功`);
-      // 如果得到 ACK 应答说明发送成功因此 prev msg 可以直接撇了
-      this.dispatch('is_ack');
-    } else if (flag === 'NAK') {
-      console.log(`客户端发送消息后接收到了 NAK 应答, 该分组发送失败, 将会重新发送`);
-      // 但是如果得到 NAK  应答说明发送失败 要重新传这个 prev msg
-      this.dispatch('is_nak', prev_msg);
+    // 在当前协议中 udp 的服务端也会返回 checksum, 因为此时的 ack 应答中其实也可能会产生错误
+    const { flag, checksum } = JSON.parse(msg);
+    if (!checksum) {
+      // ACK/NAK 应答报文在网络传输过程中其实也是可能发生错误的 此时 checksum 会不正确
+      console.log('服务端返回的 checksum 错误, 该应答回送失败, 客户端将会重新发送当前分组');
+      this.dispatch('corrupt', prev_msg);
+    } else {
+      // 进到这里说明服务端回送的数据报没有问题
+      if (flag === 'NAK') {
+        console.log('服务端返回了 NAK 应答, 客户端将会重新发送当前分组');
+        this.dispatch('is_nak', prev_msg);
+      } else if (flag === 'ACK') {
+        console.log('服务端返回了 ACK 应答, 本次分组发送成功');
+        // 如果校验和没出错同时还是 ACK 应答的话 说明该分组没毛病 可以发送下一个序号的分组了
+        this.current_seq = this.current_seq === 1 ? 0 : 1;
+        this.dispatch('is_ack');
+      }
     }
   });
 
-  /**
-   * 可能会有多个场景触发 rdt_send
-   *  1. 由上层应用(比如 setIntervel)调用
-   *    a. 调用时可能 buffer_queue 存在其他 msg 说明当前正在有一个 msg 被发送中,
-   *       此时要等上一个 msg 被发送成功后才能进入下一个 msg 的发送,
-   *       所以先把传进来的新的 msg 放到 buffer_queue 的队尾 也就是最后一个
-   *    b. 调用时 buffer_queue 中不存在其他 msg 说明可能是第一次调用 也可能是空闲时的调用,
-   *       此时直接把 msg 放进 buffer_queue 的队头 对头的 msg 就表示下一个要发送的分组的 msg,
-   *       然后走之后的逻辑 将该 msg 发送到 udp 的服务端
-   *  2. 当 udp 的服务端返回 NAK 应答时需要重新发送 msg 此时需要调用 rdt_send
-   *    a. 当接收到 NAK 的时候先使用一个临时变量将 buffer_queue 中所有的 msg 都保存出来
-   *       然后将 buffer_queue 的 length 置为 0, 这是为了下一步调用 rdt_send 的时候可以保证走到上面 1.b 中的逻辑
-   *  3. 当 udp 的服务端返回 ACK 应答的时候需要判断 buffer_queue 中是否还有排队中的 msg
-   *    a. 如果 buffer_queue 中还有在排队的 msg, 则需要也把所有的 msg 先保存一下,
-   *       然后让 buffer_queue 的 length 为 0, 也是为了保证下一步 rdt_send 的时候能够立即发送 buffer_queue[0] 的 msg
-   *    b. 如果 buffer_queue 中没有 msg 了就可以做一些其他的操作了
-   */
   dispatch = (action, msg) => {
     switch(action) {
       case this.ACTIONS.RDT_SEND:
@@ -77,58 +73,55 @@ class ClientFiniteStateMachine {
           // 需要先等之前的分组发送完成才能继续发送下一个分组
           // 把当前的 msg 先推入队列 只有当该 buffer_queue[0] 位的 msg 成功被发送到 udp 的服务端时
           // 才能把 buffer_queue[0] 真的从队列的左侧 shift 出来
-          if (Array.isArray(msg)) {
-            // 如果是个 array 类型就平铺开
-            this.buffer_queue.push(...msg);
-          } else {
-            // 如果传进来的 msg 不是 Array 就直接 push 到队尾
-            this.buffer_queue.push(msg);
-          }
+          Array.isArray(msg) ? this.buffer_queue.push(...msg) : this.buffer_queue.push(msg);
         } else {
           // 如果 buffer_queue 中没有 msg 了那就可以立即发送当前传进来的 msg 了
           // 同样也要先把它放进 buffer_queue 的最左侧缓存起来 以防止该 msg 发送失败
           // 同时 unshift 之后也能保证再 udp 服务端没有应答之前再有新的消息进来的话可以保证走到上面有 length 的逻辑
-          if (Array.isArray(msg)) {
-            // 如果是个 array 类型就平铺插到队头
-            this.buffer_queue.unshift(...msg);
-          } else {
-            // 如果传进来的 msg 不是 Array 就直接插入到队头 也就是 buffer_queue 的最左侧
-            this.buffer_queue.unshift(msg);
-          } 
-          // 由于当前不好模拟真正网络请求中校验和出错的场景 所以这里设置一个假的开关
-          const random_error_switch = Math.random() >= 0.5;
-          // 该开关为 0 时候表示校验和出现差错, 为 1 时表示校验和没有出现差错
-          const checksum = random_error_switch ? 0 : 1;
-          console.log(`本次分组随机生成的校验和是: ${checksum}`);
+          Array.isArray(msg) ? this.buffer_queue.unshift(...msg) : this.buffer_queue.unshift(msg);
           // 使用队列中最左侧的元素作为 msg 封装为 packet
-          const packet = this.make_pkt(checksum, this.buffer_queue[0]);
+          // 同时要使用 current_seq 作为一个序号 第一次是 0
+          const packet = this.make_pkt(this.current_seq, this.get_checksum(), this.buffer_queue[0]);
           // 发送该数据报
           this.udt_send(packet);
         }
         break;
-      case this.ACTIONS.IS_NAK:
-        // 如果是 NAK 应答的话说明 UDP 的服务端认为数据报或分组发生了错误 此时需要重传
-        const temp_current_msg1 = [msg, ...this.buffer_queue];
+      case this.ACTIONS.CORRUPT:
+        // 出错的话逻辑和 NAK 时的逻辑基本一样
+        const temp_current_msg1 = msg ? [msg, ...this.buffer_queue] : [...this.buffer_queue];
         this.buffer_queue.length = 0;
         this.dispatch('rdt_send', temp_current_msg1);
         break;
-      case this.ACTIONS.IS_ACK:
-        // 如果是 ACK 应答的话就可以什么都不做或是继续发送下一个分组了
+      case this.ACTIONS.NOT_CORRUPT:
+        // 没出错时的逻辑和 ACK 应答时基本一样
         if (this.buffer_queue.length) {
-          // 从队列的最左侧拿出一个 msg
           const temp_current_msg2 = [...this.buffer_queue];
           this.buffer_queue.length = 0;
           this.dispatch('rdt_send', temp_current_msg2);
         } else {
-          console.log('可以做一些别的事情了比如发送下一个分组之类的或者断开 socket');
-          // this.udp_client.close();
+          console.log('可以做一些别的事情了比如断开 socket');
+        }
+      case this.ACTIONS.IS_NAK:
+        // 如果是 NAK 应答的话说明 UDP 的服务端认为数据报或分组发生了错误 此时需要重传
+        const temp_current_msg3 = msg ? [msg, ...this.buffer_queue] : [...this.buffer_queue];
+        this.buffer_queue.length = 0;
+        this.dispatch('rdt_send', temp_current_msg3);
+        break;
+      case this.ACTIONS.IS_ACK:
+        // 如果是 ACK 应答的话就可以什么都不做或是继续发送下一个分组了
+        if (this.buffer_queue.length) {
+          const temp_current_msg4 = [...this.buffer_queue];
+          this.buffer_queue.length = 0;
+          this.dispatch('rdt_send', temp_current_msg4);
+        } else {
+          console.log('可以做一些别的事情了比如断开 socket');
         }
         break;
       default: return;
     }
   }
 
-  make_pkt = (checksum, msg) => (JSON.stringify({ data: msg, checksum }));
+  make_pkt = (seq, checksum, msg) => (JSON.stringify({ data: msg, checksum, seq }));
 
   udt_send = (pkt) => {
     // 有中文的话最好使用 Buffer 缓冲区 否则下面 send 方法的第三个参数的 length 不好判断
@@ -136,7 +129,6 @@ class ClientFiniteStateMachine {
     // 第二参数 0 表示要发送的信息在 _buffer 中的偏移量
     this.udp_client.send(_buffer, 0, _buffer.byteLength, this.SERVER_PORT, this.SERVER_ADDRESS);
   }
-
 
   // 绑定某个端口
   init_bind_port = () => this.udp_client.bind(this.CLIENT_PORT);
@@ -146,6 +138,16 @@ class ClientFiniteStateMachine {
 
   // 错误处理
   init_on_error = () => this.udp_client.on('error', (err) => console.log(`upd 服务发生错误: ${err}`));
+
+  // 生成一个假的随机的校验和
+  get_checksum = () => {
+    // 由于当前不好模拟真正网络请求中校验和出错的场景 所以这里设置一个假的开关
+    const random_error_switch = Math.random() >= 0.5;
+    // 该开关为 0 时候表示校验和出现差错, 为 1 时表示校验和没有出现差错
+    const checksum = random_error_switch ? 0 : 1;
+    console.log(`本次分组随机生成的校验和是: ${checksum}`);
+    return checksum;
+  }
 
 }
 
--- rdt2.1/udp-server.js
+++ rdt2.1/udp-server.js
@@ -1,4 +1,4 @@
-// dgram 模块提供了对 udp socket 的封装
+ // dgram 模块提供了对 udp socket 的封装
 const dgram = require('dgram');
 const SERVER_PORT = 13190;
 
@@ -8,6 +8,9 @@ class ServerFiniteStateMachine {
     NOT_CORRUPT: 'not_corrupt', // 该动作在校验和没出错的情况下触发
     CORRUPT: 'corrupt', // 该动作在校验和出错的情况下触发
   };
+
+  // 用该变量表示服务端渴望接收到的分组的序号
+  desired_seq = 0;
 
   constructor({ SERVER_PORT }) {
     if (SERVER_PORT) {
@@ -28,13 +31,25 @@ class ServerFiniteStateMachine {
   // 接收消息
   init_on_message = () => this.udp_server.on('message', (pkt, { port, address }) => {
     console.log(`${SERVER_PORT} 端口的 udp 服务接收到了来自 ${address}:${port} 的消息`);
-    const { checksum, data } = JSON.parse(pkt);
-    if (checksum) {
-      console.log(`消息的校验和 checksum 是正确的, 将返回 ACK 应答`);
+    const { seq, checksum, data } = JSON.parse(pkt);
+    if (checksum && seq === this.desired_seq) {
+      // 如果校验和没有出错并且客户端传过来的序号和服务端渴望得到的序号也一致
+      // 那就可以返回 ACK 报文
+      console.log(`消息的校验和 checksum 以及 seq 都是正确的, 将返回 ACK 应答`);
+      // 然后要修改渴望得到的序号为下一个
+      this.desired_seq = this.desired_seq === 0 ? 1 : 0;
       this.dispatch('not_corrupt', { packet: JSON.stringify(data), port, address });
     } else {
-      console.log(`消息的校验和 checksum 发生了错误, 将返回 NAK 应答`);
-      this.dispatch('corrupt', { port, address });
+      if (!checksum) {
+        // 如果校验和出错说明客户端传过来的数据本身可能出现问题了
+        console.log(`消息的校验和 checksum 出错, 将返回 NAK 应答`);
+        this.dispatch('corrupt', { port, address });
+      } else if (seq !== this.desired_seq) {
+        // 如果校验和没错但是传过来的序号不是期望得到的
+        // 说明可能在上次一服务端往客户端传送应答时的那份儿应答挂了
+        // 此时需要重新回传一份 ACK
+        this.dispatch('not_corrupt', { packet: JSON.stringify(data), port, address });
+      }
     }
   });
 
@@ -48,7 +63,7 @@ class ServerFiniteStateMachine {
         break;
       case this.ACTIONS.CORRUPT:
         // 如果发生了错误的话就构建一个 NAK 错误应答的报文
-        const sndpkt1 = this.make_pkt('NAK');
+        const sndpkt1 = this.make_pkt('NAK', this.get_checksum());
         // 并且把这个 NAK 的否定应答返回给客户端
         this.udt_send(sndpkt1, { port, address });
         break;
@@ -56,7 +71,7 @@ class ServerFiniteStateMachine {
         // 如果状态是 not corrupt 说明客户端发送过来的报文的校验和是正确的
         this.dispatch('rdt_rcv', { packet, port, address });
         // 此时就要构建一个 ACK 应答表示成功接收到了数据报或分组
-        const sndpkt2 = this.make_pkt('ACK');
+        const sndpkt2 = this.make_pkt('ACK', this.get_checksum());
         // 然后将成功应答返回给客户端
         this.udt_send(sndpkt2, { port, address });
         break;
@@ -65,7 +80,8 @@ class ServerFiniteStateMachine {
   }
 
   // flag 表示 NAK 或 ACK 标志位
-  make_pkt = (flag, msg) => (JSON.stringify({ data: msg, flag }));
+  // 由于返回的应答报文实际上也可能会发生错误 所以也需要有个 checksum
+  make_pkt = (flag, checksum, msg) => (JSON.stringify({ data: msg, flag, checksum }));
 
   extract = (packet) => (JSON.parse(packet));
 
@@ -81,7 +97,7 @@ class ServerFiniteStateMachine {
     // 第二参数 0 表示要发送的信息在 _buffer 中的偏移量
     this.udp_server.send(_buffer, 0, _buffer.byteLength, port, address);
   }
-  
+
   // 绑定端口
   init_bind_port = () => this.udp_server.bind(this.SERVER_PORT);
 
@@ -93,6 +109,17 @@ class ServerFiniteStateMachine {
     console.log(`upd 服务发生错误: ${err}`);
     this.udp_server.close();
   });
+
+  // 生成一个假的随机的校验和
+  get_checksum = () => {
+    // 由于当前不好模拟真正网络请求中校验和出错的场景 所以这里设置一个假的开关
+    const random_error_switch = Math.random() >= 0.5;
+    // 该开关为 0 时候表示校验和出现差错, 为 1 时表示校验和没有出现差错
+    const checksum = random_error_switch ? 0 : 1;
+    console.log(`本次分组随机生成的校验和是: ${checksum}`);
+    return checksum;
+  }
+
 }
 
 const SFSM = new ServerFiniteStateMachine({ SERVER_PORT });
